8d1535e4b7d7a2ee894adb3850fb3c0f65fbd155
Tal Levy <tallevy16@gmail.com>
2025-05-21 14:06:04 +0300

    Implement player name editing functionality in Player component

--- 04-tic-tac-toe/src/components/Player.jsx
+++ 04-tic-tac-toe/src/components/Player.jsx
@@ -1,11 +1,27 @@
-export default function Player({name, symbol}) {
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  let buttonCaption = "Edit";
+
+  if (isEditing) {
+    playerName = <input type="text" required value={name}/>;
+    buttonCaption = "Save";
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">Player {name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{buttonCaption}</button>
     </li>
   );
 }
