4462a018ad03ec53fb833abc02779a1956e64722
Tal Levy <tallevy16@gmail.com>
2025-05-21 14:51:06 +0300

    Add GameBoard component to display the game board layout

--- 04-tic-tac-toe/src/App.jsx
+++ 04-tic-tac-toe/src/App.jsx
@@ -1,7 +1,7 @@
 
 
 import Player from "./components/Player";
-
+import GameBoard from "./components/GameBoard";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
           <Player initialName="Player 1" symbol="X"/>
           <Player initialName="Player 2" symbol="O"/>
         </ol>
-        GAME BOARD
+        <GameBoard />
       </div>
       LOG
     </main>
