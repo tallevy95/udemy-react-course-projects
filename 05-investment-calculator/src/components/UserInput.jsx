f9d3049200da0e822c8623016a99e6d8707aac17
Tal Levy <tallevy16@gmail.com>
2025-05-26 15:11:07 +0300

    Refactor INPUT_GROUPS structure for improved readability and consistency

--- 05-investment-calculator/src/components/UserInput.jsx
+++ 05-investment-calculator/src/components/UserInput.jsx
@@ -1,26 +1,22 @@
 import InputField from './InputField';
 
 const INPUT_GROUPS = [
-    {
-      group: [
-        { label: "Initial Investment", id: "initialInvestment" },
-        { label: "Annual Investment", id: "annualInvestment" }
-      ]
-    },
-    {
-      group: [
-        { label: "Expected Return", id: "expectedReturn" },
-        { label: "Duration", id: "duration" }
-      ]
-    }
-  ];
+  [
+    { label: "Initial Investment", id: "initialInvestment" },
+    { label: "Annual Investment", id: "annualInvestment" }
+  ],
+  [
+    { label: "Expected Return", id: "expectedReturn" },
+    { label: "Duration", id: "duration" }
+  ]
+];
 
 export default function UserInput({onChange, userInput}) {
   return (
     <section className="user-input">
-        {INPUT_GROUPS.map((group, index) => (
-            <div key={index} className='input-group'>
-                {group.group.map(field => (
+        {INPUT_GROUPS.map((fieldGroup, groupIndex) => (
+            <div key={groupIndex} className='input-group'>
+                {fieldGroup.map(field => (
                     <InputField 
                     key={field.id}
                     label={field.label}
