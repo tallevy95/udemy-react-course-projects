b7f1c7a3f17333060ddcf16f707cc0fad6935c0e
Tal Levy <tal.levy-ext@next-insurance.com>
2025-06-28 16:19:54 +0300

    Display score in ResultModal when the user does not lose

--- 08-refs/src/components/ResultModal.jsx
+++ 08-refs/src/components/ResultModal.jsx
@@ -10,6 +10,7 @@ export default function ResultModal({
 
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -22,6 +23,7 @@ export default function ResultModal({
   return (
     <dialog ref={dialog} className="result-modal">
       {userLost && <h2>You lost!</h2>}
+      {!userLost && <h2>Yout Score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
