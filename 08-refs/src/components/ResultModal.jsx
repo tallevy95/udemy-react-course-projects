6045b352219738d3df0e08835f2b216ff716a819
Tal Levy <tal.levy-ext@next-insurance.com>
2025-06-28 16:20:26 +0300

    Add onClose handler to ResultModal dialog for improved modal behavior (closeing the window with ESC key)

--- 08-refs/src/components/ResultModal.jsx
+++ 08-refs/src/components/ResultModal.jsx
@@ -21,7 +21,7 @@ export default function ResultModal({
   });
 
   return (
-    <dialog ref={dialog} className="result-modal">
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {userLost && <h2>You lost!</h2>}
       {!userLost && <h2>Yout Score: {score}</h2>}
       <p>
